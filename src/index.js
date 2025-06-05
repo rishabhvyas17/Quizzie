a2386e297663229609300e8f1e6ef061d187f38e
Prince Yadav <princeyadav810py@gmail.com>
2025-06-06 01:18:10 +0530

    added student/teacher toggle functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const path=require("path")
 const hbs=require("hbs")
-const collection =require("./mongodb")
+const {studentCollection, teacherCollection} =require("./mongodb") // added teacher and student collection
 
 const templatePath=path.join(__dirname,'../tempelates')
 
@@ -21,43 +21,61 @@ app.get("/signup",(req,res)=>{
     res.render("signup")
 })
 
-app.post("/signup",async (req,res)=>{
+app.post("/signup", async (req, res) => {
+    try {
+        // Create a new user object with data from the request
+        const data = {
+            name: req.body.name,
+            password: req.body.password
+        }
 
-const data ={
-    name:req.body.name,
-    password:req.body.password
+        // Check the user type from the form and insert into appropriate collection
+        const userType = req.body.userType; // 'student' or 'teacher'
+        
+        if (userType === 'teacher') {
+            await teacherCollection.insertMany([data])
+        } else {
+            await studentCollection.insertMany([data])
+        }
 
-}
-
-await collection.insertMany([data])
-
-// res.render("home")
-res.redirect("/login")
-
+        // Redirect to home page after successful registration
+        res.render("home", { userType: userType })
+    } catch (error) {
+        console.log(error)
+        res.send("Error during registration")
+    }
 })
 
+// app.get("/", (req, res) => {
+//     res.render("home");
+// });
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+// Handle user login
+app.post("/login", async (req, res) => {
+    try {
+        const { name, password, userType } = req.body;
+        let user;
 
-app.post("/login",async (req,res)=>{
-
-    try{
-        const check = await collection.findOne({name:req.body.name})
-
-        if(check.password === req.body.password){
-            
-            res.redirect("/")
+        // Check in appropriate collection based on user type
+        if (userType === 'teacher') {
+            user = await teacherCollection.findOne({ name: name })
+        } else {
+            user = await studentCollection.findOne({ name: name })
         }
-        else{
-            res.send("wrong password")
-        }
-    }
-    catch{
-        res.send("wrong details")
-    }
 
+        // Check if user exists and password matches
+        if (user && user.password === password) {
+            res.render("home", { 
+                userType: userType,
+                userName: user.name 
+            })
+        } else {
+            res.send("Wrong credentials")
+        }
+    } catch (error) {
+        console.log(error)
+        res.send("Login failed")
+    }
 })
 
 
