edde5d0fd1a8103cfc94e09f7ac06a6b0d7d6e19
rishabhvyas17 <rishabhvyas222@gmail.com>
2025-06-05 12:15:19 +0530

    Initial commit - login/signup page

---
+++ src/mongodb.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/LoginSignup")
+.then(()=>{
+    console.log("mongodb connected");
+})
+
+.catch(()=>{
+    console.log("failed to connect");
+})
+
+const LogInSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+})
+
+const collection= new mongoose.model("Collecion1",LogInSchema )
+module.exports = collection
